6c395365c516055e991b3f88f1c5b1452fae6aca
João Gusmão <joao@natsce.com>
2018-08-03 11:09:21 -0300

    Cleaning code comments

--- src/index.jsx
+++ src/index.jsx
@@ -1,16 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import App from './App';
-import Login from './Login';
-
-// ReactDOM.render(
-// <BrowserRouter>
-//   <Switch>
-//     <Route path="/" exact={true} component={Login} />
-//     <Route path="/find" component={App} />
-//   </Switch>
-// </ BrowserRouter>, document.getElementById('app'));
 
 ReactDOM.render(<App />, document.getElementById('app'));
 
